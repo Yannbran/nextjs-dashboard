db8fd153f002b5b8ad272e33e0d3ca79057e60e6
Yann <yannbran@gmail.com>
2024-11-15 14:44:21 +0100

    maj

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,13 +1,12 @@
 // import bcrypt from 'bcrypt';
-// import { sql } from '@vercel/postgres';
+// import { db } from '@vercel/postgres';
 // import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
-// const client = await sql.connect({ 
-//   connectionString: process.env.DATABASE_URL, });
+// const client = await db.connect();
 
-// // async function seedUsers() {
-//   // await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-//   // await client.sql`
+// async function seedUsers() {
+//   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+//   await client.sql`
 //     CREATE TABLE IF NOT EXISTS users (
 //       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
 //       name VARCHAR(255) NOT NULL,
@@ -102,22 +101,23 @@
 //   return insertedRevenue;
 // }
 
-// export async function GET() {
-//   return Response.json({
-//     message:
-//       'Uncomment this file and remove this line. You can delete this file when you are finished.',
-//   });
-//   try {
-//     await client.sql`BEGIN`;
-//     await seedUsers();
-//     await seedCustomers();
-//     await seedInvoices();
-//     await seedRevenue();
-//     await client.sql`COMMIT`;
-
-//     return Response.json({ message: 'Database seeded successfully' });
-//   } catch (error) {
-//     await client.sql`ROLLBACK`;
-//     return Response.json({ error }, { status: 500 });
-//   }
-// }
+export async function GET() {
+    return Response.json({
+      message:
+        'Uncomment this file and remove this line. You can delete this file when you are finished.',
+    });
+    // try {
+    //   await client.sql`BEGIN`;
+    //   await seedUsers();
+    //   await seedCustomers();
+    //   await seedInvoices();
+    //   await seedRevenue();
+    //   await client.sql`COMMIT`;
+  
+    //   return Response.json({ message: 'Database seeded successfully' });
+    // } catch (error) {
+    //   await client.sql`ROLLBACK`;
+    //   return Response.json({ error }, { status: 500 });
+    // }
+  }
+  
